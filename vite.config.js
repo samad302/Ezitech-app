031ab95ea250dc0e33692b6d47eb8bbdeb90c7ae
samad302 <sammadjawaid465@gmail.com>
2025-02-16 08:35:21 +0500

    first commit

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: "/Ezitech-app/", // Corrected the base URL
+
   plugins: [react()],
 })
